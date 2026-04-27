45a88fac3428c4e6ca5f00a92d0cdd7438ef576d
Sanjana <sanjana@srv1489436.hstgr.cloud>
2026-04-27 03:04:20 +0000

    Done

--- apps/cms-backend/generated/prisma/index.js
+++ apps/cms-backend/generated/prisma/index.js
@@ -227,7 +227,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "D:\\Files\\KaraliyaddaOne\\jlracing\\apps\\cms-backend\\generated\\prisma",
+      "value": "/root/projects/jlracing/jlracing/apps/cms-backend/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -236,16 +236,16 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "windows",
+        "value": "debian-openssl-3.0.x",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "D:\\Files\\KaraliyaddaOne\\jlracing\\apps\\cms-backend\\prisma\\schema.prisma",
+    "sourceFilePath": "/root/projects/jlracing/jlracing/apps/cms-backend/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
@@ -255,7 +255,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
@@ -303,8 +302,8 @@ exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
-path.join(__dirname, "query_engine-windows.dll.node");
-path.join(process.cwd(), "generated/prisma/query_engine-windows.dll.node")
+path.join(__dirname, "libquery_engine-debian-openssl-3.0.x.so.node");
+path.join(process.cwd(), "generated/prisma/libquery_engine-debian-openssl-3.0.x.so.node")
 // file annotations for bundling tools to include these files
 path.join(__dirname, "schema.prisma");
 path.join(process.cwd(), "generated/prisma/schema.prisma")
