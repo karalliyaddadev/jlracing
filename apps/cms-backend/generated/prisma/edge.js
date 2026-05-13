6da25a0f805050da2b9befc3c98fdc7b04eda848
Sanjana <sanjana@srv1489436.hstgr.cloud>
2026-05-13 03:54:28 +0000

    done

--- apps/cms-backend/generated/prisma/edge.js
+++ apps/cms-backend/generated/prisma/edge.js
@@ -247,7 +247,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "D:\\Files\\KaraliyaddaOne\\jlracing\\apps\\cms-backend\\generated\\prisma",
+      "value": "/root/projects/jlracing/jlracing/apps/cms-backend/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -256,12 +256,12 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "windows",
+        "value": "debian-openssl-3.0.x",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "D:\\Files\\KaraliyaddaOne\\jlracing\\apps\\cms-backend\\prisma\\schema.prisma",
+    "sourceFilePath": "/root/projects/jlracing/jlracing/apps/cms-backend/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
--- apps/cms-backend/generated/prisma/index.js
+++ apps/cms-backend/generated/prisma/index.js
@@ -248,7 +248,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "D:\\Files\\KaraliyaddaOne\\jlracing\\apps\\cms-backend\\generated\\prisma",
+      "value": "/root/projects/jlracing/jlracing/apps/cms-backend/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -257,12 +257,12 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "windows",
+        "value": "debian-openssl-3.0.x",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "D:\\Files\\KaraliyaddaOne\\jlracing\\apps\\cms-backend\\prisma\\schema.prisma",
+    "sourceFilePath": "/root/projects/jlracing/jlracing/apps/cms-backend/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
@@ -324,8 +324,8 @@ exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
-path.join(__dirname, "query_engine-windows.dll.node");
-path.join(process.cwd(), "generated/prisma/query_engine-windows.dll.node")
+path.join(__dirname, "libquery_engine-debian-openssl-3.0.x.so.node");
+path.join(process.cwd(), "generated/prisma/libquery_engine-debian-openssl-3.0.x.so.node")
 // file annotations for bundling tools to include these files
 path.join(__dirname, "schema.prisma");
 path.join(process.cwd(), "generated/prisma/schema.prisma")
