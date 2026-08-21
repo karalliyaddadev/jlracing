a317503ea0e5a129006d817297b99828457d9c8a
Sanjana <sanjana@srv1489436.hstgr.cloud>
2026-08-21 14:41:13 +0000

    sub acc

--- apps/backend/src/generated/prisma/edge.js
+++ apps/backend/src/generated/prisma/edge.js
@@ -685,7 +685,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "D:\\Files\\KaraliyaddaOne\\jlracing\\apps\\backend\\src\\generated\\prisma",
+      "value": "/root/projects/jlracing/jlracing-live/jlracing/apps/backend/src/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -694,12 +694,12 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "windows",
+        "value": "debian-openssl-3.0.x",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "D:\\Files\\KaraliyaddaOne\\jlracing\\apps\\backend\\prisma\\schema.prisma",
+    "sourceFilePath": "/root/projects/jlracing/jlracing-live/jlracing/apps/backend/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
